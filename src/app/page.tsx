cd43d282f6d61b0fac844ff33c5aa823ca9d015c
Joseph Bangoura <ptijjo@hotmail.com>
2024-02-03 13:25:03 +0100

    update transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,48 +8,11 @@ import Contact from "@/components/Contact";
 import Resultats from "@/components/Resultats";
 import Footer from "@/components/Footer";
 import Reseau from "@/components/Reseau";
-import ScrollReveal from 'scrollreveal';
+
 
 
 const Home: React.FC = () => {
-  const initScrollReveal = () => {
-    ScrollReveal().reveal('.presentation', {
-      duration: 1000,
-      //easing: 'ease-in-out',
-      //reset: true,
-    });
-
-    ScrollReveal().reveal('.resultats', {
-      // Options de configuration
-      duration: 1000,
-      //easing: 'ease-in-out',
-      //reset: true,
-      delay: 500
-    });
-
-    ScrollReveal().reveal('.products', {
-      // Options de configuration
-      duration: 1000,
-      //easing: 'ease-in-out',
-      //reset: true,
-      delay: 500
-    });
-
-    ScrollReveal().reveal('.contact', {
-      // Options de configuration
-      duration: 1000,
-      //easing: 'ease-in-out',
-      //reset: true,
-      delay: 500
-    });
-  };
-
-  // Appel de la fonction d'initialisation dans le cycle de vie de votre composant
-  React.useEffect(() => {
-    initScrollReveal();
-  }, []);
-
-
+  
   return (
     <>
       <Header />
