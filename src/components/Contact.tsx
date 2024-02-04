66aeeb4fd864a9d7ea440087e0545d6e363f0db6
Joseph Bangoura <ptijjo@hotmail.com>
2024-02-04 15:28:06 +0100

    forme presentation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,7 +9,6 @@ const Contact: React.FC = () => {
 
     return (
         <div className='contact' id='contact'>
-            <h2>Cliquez ici pour nous rejoindre</h2>
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSfSZYpyMS4tyGhVEXaezb6UCttO9tJI2a3vEnmY96U1F_Icng/viewform" rel="noopener noreferrer" target='_blank' aria-label="Lien pour le bilan"><p className='btn-bilan'>Rejoignez nous</p> </a>
 
         </div>
