83c20501c901cec4dc7d657ec74f11b899a25a41
Joseph Bangoura <ptijjo@hotmail.com>
2024-02-15 22:07:09 +0100

    rectification next configjs

--- next.config.js
+++ next.config.js
@@ -1,31 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-export default nextConfig
-
-/*const path = require('path');
-
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.forEach((rule) => {
-        if (String(rule.test) === '/\\.css$/') {
-          rule.use.push({
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                config: './postcss.config.js',
-              },
-            },
-          });
-        }
-      });
-    }
-    return config;
-  },
-};*/
-
+export default nextConfig;
 
